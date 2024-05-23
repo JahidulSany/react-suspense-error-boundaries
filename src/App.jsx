3c6b5d7a7ef85d1b76fccd9d049814a2f16877ce
JahidulSany <jahidulsanypro@gmail.com>
2024-05-23 22:15:09 +0100

    Suspense Enabled PostSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 import './App.css';
 import Comments from './components/Comments';
 import PostSelector from './components/PostSelector';
 export default function App() {
   const [selectedPostId, setSelectedPostId] = useState(null);
-  
+
   const handleSelectPost = (e) => {
     setSelectedPostId(e.target.value);
   };
   return (
     <>
-      <h1 className='text-3xl mb-2'>React Suspense and Error Boundaries</h1>
-      <div className='text-2xl'>
-        <PostSelector onSelectPost={handleSelectPost} />
+      <h1 className="text-3xl mb-2">React Suspense and Error Boundaries</h1>
+      <div className="text-2xl">
+        <Suspense fallback={<h1>Loading Posts...</h1>}>
+          <PostSelector onSelectPost={handleSelectPost} />
+        </Suspense>
         {selectedPostId && <Comments postId={selectedPostId} />}
       </div>
     </>
---
+++ src/utils/wrapPromise.js
@@ -0,0 +1,28 @@
+export default function wrapPromise(promise) {
+  let status = 'pending';
+  let response;
+
+  const suspender = promise.then(
+    (res) => {
+      (status = 'success'), (response = res);
+    },
+    (error) => {
+      (status = 'error'), (response = error);
+    },
+  );
+
+  const read = () => {
+    switch (status) {
+      case 'pending':
+        throw suspender;
+      case 'error':
+        throw error;
+      default:
+        return response;
+    }
+  };
+
+  return {
+    read,
+  };
+}
