bee32cc3e621a4e5e4a98f681b2085383ab9916c
JahidulSany <jahidulsanypro@gmail.com>
2024-05-23 23:11:14 +0100

    Error boundaries

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import { Suspense, useState } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
 import Comments from './components/Comments';
 import PostSelector from './components/PostSelector';
+
 export default function App() {
   const [selectedPostId, setSelectedPostId] = useState(null);
 
   const handleSelectPost = (e) => {
     setSelectedPostId(e.target.value);
   };
+
   return (
-    <>
-      <h1 className="text-3xl mb-2">React Suspense and Error Boundaries</h1>
-      <div className="text-2xl">
-        <Suspense fallback={<h1>Loading Posts...</h1>}>
-          <PostSelector onSelectPost={handleSelectPost} />
-        </Suspense>
-        {selectedPostId && (
-          <Suspense fallback={<h1>Loading Comments...</h1>}>
-            <Comments postId={selectedPostId} />
+    <div>
+      <h1>React Suspense and Error Boundaries</h1>
+
+      <div>
+        <ErrorBoundary
+          fallback={<h1 className="error">There was an error occured!</h1>}
+        >
+          <Suspense fallback={<h1>Loading posts...</h1>}>
+            <PostSelector onSelectPost={handleSelectPost} />
           </Suspense>
-        )}
+
+          {selectedPostId && (
+            <ErrorBoundary
+              fallback={
+                <h1 className="error">There was an error fetching comments</h1>
+              }
+            >
+              <Suspense fallback={<h1>Loading comments...</h1>}>
+                <Comments postId={selectedPostId} />
+              </Suspense>
+            </ErrorBoundary>
+          )}
+        </ErrorBoundary>
       </div>
-    </>
+    </div>
   );
 }
+Í;
